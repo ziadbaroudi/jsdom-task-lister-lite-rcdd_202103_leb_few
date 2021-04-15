7bc06aed7d482d97c32c7341c274d124a6fbafb9
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-15 00:45:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  
+  // function new_el(){
+    
+    let subbtn= document.getElementById("subbtn");
+    subbtn.addEventListener("click", (e) =>{
+  
+    const ls = document.getElementById("tasks");
+    let li = document.createElement("LI");
+    let inp = document.getElementById("new-task-description").value;
+    let n = document.createTextNode(inp);
+    li.innerText += inp ;
+    li.appendChild(n);
+    document.getElementById("list").appendChild(li);
+    event.preventDefault();
+    });
+    
+  
+    // }
+    
 });
